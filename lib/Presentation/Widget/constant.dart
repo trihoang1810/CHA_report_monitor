56a8fe553a52a60a51b36e35e3ba5c5acc1bbefc
Minh Tri <tri.hoang.hcmut@gmail.com>
2021-07-05 17:20:04 +0700

    sua loi insecure HTTP

--- lib/Presentation/Widget/constant.dart
+++ lib/Presentation/Widget/constant.dart
@@ -4,7 +4,7 @@ double screenWidthGlobal;
 
 class Constants {
   static const String baseUrl =
-      "https://phong-kiem-tra-chat-luong-sp.herokuapp.com";
+      "http://192.168.1.13:8083";
   static const Color mainColor = Color(0xff001D37);
   static const Color secondaryColor = Color(0xff00294D);
   static const Duration timeOutLimitation = Duration(seconds: 10);
